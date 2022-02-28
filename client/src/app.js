b357285264a6520f8879b466587ea53f32116294
mountaint0p <summit100@gmail.com>
2022-02-27 19:29:13 -0500

    Added Light and Dark Mode

--- client/src/app.js
+++ client/src/app.js
@@ -2,24 +2,28 @@ import { React, useState } from "react";
 import { Heading, VStack } from "@chakra-ui/react";
 import TodoList from "./components/TodoList";
 import AddTodo from "./components/AddTodo";
+import LightModeButton from "./components/LightModeButton";
 
 function App() {
 	const [todos, setTodos] = useState([]);
 	return (
-		<VStack p={4}>
-			<Heading
-				size="2xl"
-				fontWeight="extrabold"
-				bgGradient="linear(to-r, pink.500, blue.500)"
-				bgClip="text"
-				pb={6}
-				pt={6}
-			>
-				Todo List
-			</Heading>
-			<TodoList todos={todos} setTodos={setTodos} />
-			<AddTodo todos={todos} setTodos={setTodos} />
-		</VStack>
+		<div>
+			<VStack p={4}>
+				<LightModeButton />
+				<Heading
+					size="2xl"
+					fontWeight="extrabold"
+					bgGradient="linear(to-r, pink.500, blue.500)"
+					bgClip="text"
+					pb={6}
+					pt={6}
+				>
+					Todo List
+				</Heading>
+				<TodoList todos={todos} setTodos={setTodos} />
+				<AddTodo todos={todos} setTodos={setTodos} />
+			</VStack>
+		</div>
 	);
 }
 
--- client/src/components/LightModeButton.js
+++ client/src/components/LightModeButton.js
@@ -3,22 +3,23 @@ import { IconButton, useColorMode } from "@chakra-ui/react";
 import { FaSun, FaMoon } from "react-icons/fa";
 
 function LightModeButton() {
-  const { colorMode, toggleColorMode } = useColorMode();
-  const [colorIcon, setColorIcon] = React.useState(<FaSun />);
-  React.useEffect(() => {
-    if (colorMode === "light") setColorIcon(<FaMoon />);
-    else setColorIcon(<FaSun />);
-  }, [colorMode]);
-  return (
-    <>
-      <IconButton
-        icon={colorIcon}
-        isRound="true"
-        size="1g"
-        onClick={toggleColorMode}
-      />
-    </>
-  );
+	const { colorMode, toggleColorMode } = useColorMode();
+	const [colorIcon, setColorIcon] = React.useState(<FaSun />);
+	React.useEffect(() => {
+		if (colorMode === "light") setColorIcon(<FaMoon />);
+		else setColorIcon(<FaSun />);
+	}, [colorMode]);
+	return (
+		<>
+			<IconButton
+				icon={colorIcon}
+				isRound="true"
+				size="lg"
+				onClick={toggleColorMode}
+				alignSelf="flex-end"
+			/>
+		</>
+	);
 }
 
 export default LightModeButton;
